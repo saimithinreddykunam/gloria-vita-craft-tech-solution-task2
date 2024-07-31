91e641e9c06607506cdd4f1151e70189dcc00092
saimithinreddykunam <kunamsaireddy76@gmail.com>
2024-07-31 22:30:50 +0530

    Create employee.java

---
+++ employee.java
@@ -0,0 +1,16 @@
+import javax.persistence.*;
+
+@Entity
+public class Employee {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+    private String email;
+    private String department;
+    private String contactDetails;
+
+    // Getters and Setters
+}
